2bdad815fcf7f2782f23cebf7c3205810d2ced6d
mzlamal <milan.zlamal@ceskatelevize.cz>
2024-05-19 23:18:41 +0200

    test: -add test db

--- tests/test_db.py
+++ tests/test_db.py
@@ -11,17 +11,17 @@ class TestDatabase(unittest.TestCase):
         self.engine = create_engine('sqlite:///:memory:')
         Base.metadata.create_all(bind=self.engine)
         self.Session = sessionmaker(bind=self.engine)
-        self.session = self.Session()
+        self.session_instance = self.Session()
 
     def tearDown(self):
-        self.session.close()
+        self.session_instance.close()
 
     def test_stock_model(self):
         stock = Stock(name="Test Stock", price=123.45, change=1.23)
-        self.session.add(stock)
-        self.session.commit()
+        self.session_instance.add(stock)
+        self.session_instance.commit()
         
-        result = self.session.query(Stock).filter_by(name="Test Stock").first()
+        result = self.session_instance.query(Stock).filter_by(name="Test Stock").first()
         self.assertEqual(result.price, 123.45)
         self.assertEqual(result.change, 1.23)
 
