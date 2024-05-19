23416ee57531d3a243c407220716357b60382ad0
mzlamal <milan.zlamal@ceskatelevize.cz>
2024-05-19 23:15:58 +0200

    feat: -add test scraper update

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -4,8 +4,15 @@ import unittest
 from src.scraper import fetch_stock_data
 
 class TestScraper(unittest.TestCase):
-
+    """
+    Test Case for Scraper
+    """
     def test_fetch_stock_data(self):
+        """
+        Test case for fetch_stock_data function.
+        This test case checks if the function returns a list of dictionaries,
+        each containing 'name', 'price', and 'change' keys.
+        """
         url = "https://www.kurzy.cz/akcie-cz/burza/"
         data = fetch_stock_data(url)
         self.assertIsInstance(data, list)
