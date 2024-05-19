4f98cec93630eed40c2597e081989b9f57de2738
mzlamal <milan.zlamal@ceskatelevize.cz>
2024-05-19 23:06:45 +0200

    feat: - add utils

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+# src/utils.py
+
+import logging
+
+def setup_logger(name: str) -> logging.Logger:
+    """Sets up a logger with the given name."""
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
